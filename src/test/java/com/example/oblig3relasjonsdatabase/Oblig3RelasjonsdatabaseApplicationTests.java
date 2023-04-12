e8128d9ef6f6933e56c6dcf32ae54a4fab8c07b0
TheresePinkLizard <therese.trollbu@gmail.com>
2023-04-12 16:56:46 +0200

    ,knjkn

---
+++ src/test/java/com/example/oblig3relasjonsdatabase/Oblig3RelasjonsdatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig3relasjonsdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig3RelasjonsdatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
